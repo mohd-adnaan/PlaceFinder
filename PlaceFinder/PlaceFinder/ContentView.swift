4044f2086229643a40bdb34449164ba715376bc8
mohd-adnaan <iamadnaanmohd@gmail.com>
2026-01-22 16:13:27 -0500

    add iOS Xcode project structure

---
+++ PlaceFinder/PlaceFinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlaceFinder
+//
+//  Created by Mohammad Adnaan on 2026-01-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlaceFinder/PlaceFinder/PlaceFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceFinderApp.swift
+//  PlaceFinder
+//
+//  Created by Mohammad Adnaan on 2026-01-22.
+//
+
+import SwiftUI
+
+@main
+struct PlaceFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
